3cc095a2078796a7109668efae4ca02244c29c29
Christian Koep <christiankoep@gmail.com>
2016-04-01 22:20:09 +0200

    Improved docker compilation method
    By default a container’s file system persists even after the container
    exits. This makes debugging a lot easier (since you can inspect the
    final state) and you retain all your data by default. But if you are
    running short-term foreground processes, these container file systems
    can really pile up. If instead you’d like Docker to automatically clean
    up the container and remove the file system when the container exits,
    you can add the --rm flag:
    
    See https://docs.docker.com/engine/reference/run/#clean-up-rm

--- compile-docker.sh
+++ compile-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker run -i -t -w '/mnt' -v `pwd`:/mnt koep/texlive /bin/bash -c './compile.sh'
+sudo docker run --rm -i -t -w '/mnt' -v `pwd`:/mnt koep/texlive /bin/bash -c './compile.sh'
