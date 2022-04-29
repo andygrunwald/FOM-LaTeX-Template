58f15f9ab1171a573a34e041116588fcf8bb4aea
BlueberryDuck <>
2022-04-29 14:06:21 +0200

    fixed countwords, at least for me

--- countwords.sh
+++ countwords.sh
@@ -1,8 +1,6 @@
 echo "Um dieses Skript zu nutzen muss detex auf dem Rechner installiert sein"
 echo "Unter macOS kann es mit dem folgenden Befehl installiert werden: brew install opendetex"
 
-rm wordcounts.txt 2> /dev/null
-
 detex thesis_main.tex > thesis_main.txt
 
 # Removes everything before Einleitung and after Anhang
@@ -11,7 +9,7 @@ ENDING="Anhang"
 while read p; do
   # end txt when end was found
   echo "$p" | grep $ENDING && break
-  if [ "$FOUNDBEGIN" == "y" ]
+  if [ "$FOUNDBEGIN" = "y" ]
       then
           echo "$p" >>wordcounts.txt
           # echo "$p" | sed 's/\[[\w. ]*\][[\w. ]*\]\w+\.\w+/ /g' >>wordcounts.txt
@@ -23,5 +21,6 @@ done <thesis_main.txt
 echo
 echo "Anzahl der Wörter vom Kapitel Einleitung bis Anhang:"
 wc -w wordcounts.txt # zeilen, wörter, zeichen
+
 rm wordcounts.txt 2> /dev/null
 rm thesis_main.txt 2> /dev/null
